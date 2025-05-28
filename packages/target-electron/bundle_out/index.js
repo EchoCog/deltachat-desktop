0b793c085e1a312175f271169a3c076b308718bf
drzo <d@rzo.io>
2025-05-28 15:42:19 +0200

    v455

--- packages/target-electron/bundle_out/index.js
+++ packages/target-electron/bundle_out/index.js
@@ -5226,7 +5226,7 @@ init_cjs_shim();
 
 // src/get-build-info.ts
 init_cjs_shim();
-var BuildInfo = JSON.parse('{"VERSION":"1.58.2","BUILD_TIMESTAMP":1748139199399,"GIT_REF":"v1.48.0-652-gb3ab4ac4a"}');
+var BuildInfo = JSON.parse('{"VERSION":"1.58.2","BUILD_TIMESTAMP":1748439407717,"GIT_REF":"v1.48.0-660-gd751718d6"}');
 
 // src/deltachat/stdio_server.ts
 import { spawn } from "child_process";
